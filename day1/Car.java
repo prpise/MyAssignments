54e0e80533371eb196f9af8c608d9d6d20ea3668
padmapriya <52442460+prpise@users.noreply.github.com>
2023-08-16 00:24:13 +0530

    Add files via upload
    week2.Day1

--- day1/Car.java
+++ day1/Car.java
@@ -1,9 +1,43 @@
-package week1.day1;
+package week2.day1;
 
-public class Car{
-
-	public static void main(String[] args) {
-		System.out.println("Welcome everyone");
+public class Car {
+	
+	
+	public void driveCar() {
+		String name = "FORD";
+		System.out.println("I am driving" + " " + name + " " );
+			
+		}
+	
+	private void applybrake() {
+		System.out.println("I am applying brake");
 	}
+	
+	protected void soundHorn() {
+		
+		System.out.println("SoundHorn");
+		
+	}
+		
+    public void isPuncture() {
+    	System.out.println(false);
+
+    }
+    
+   
+
+public static void main(String []args) {
+	
+	Car obj=new Car();
+	obj.driveCar();
+	obj.applybrake();
+	obj.soundHorn();
+	obj.isPuncture();
+	
+}
 
 }
+
+ 
+
+
