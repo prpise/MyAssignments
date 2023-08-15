db4b7e5e3b6938618ceeda423a0386de6b22e5e1
padmapriya <52442460+prpise@users.noreply.github.com>
2023-08-16 00:41:04 +0530

    Assignments
    day3

---
+++ day3/Car.java
@@ -0,0 +1,43 @@
+package week2.day3;
+
+public class Car {
+	
+	
+	public void driveCar() {
+		String name = "FORD";
+		System.out.println("I am driving" + " " + name + " " );
+			
+		}
+	
+	private void applybrake() {
+		System.out.println("I am applying brake");
+	}
+	
+	protected void soundHorn() {
+		
+		System.out.println("SoundHorn");
+		
+	}
+		
+    public void isPuncture() {
+    	System.out.println(false);
+
+    }
+    
+   
+
+public static void main(String []args) {
+	
+	Car obj=new Car();
+	obj.driveCar();
+	obj.applybrake();
+	obj.soundHorn();
+	obj.isPuncture();
+	
+}
+
+}
+
+ 
+
+
---
+++ day3/Class.java
@@ -0,0 +1,14 @@
+package week2.day3;
+
+public class Class {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Mobile obj = new Mobile();
+		obj.sendSMS();
+		obj.allowVoiceCall();
+	
+		
+	}
+
+}
